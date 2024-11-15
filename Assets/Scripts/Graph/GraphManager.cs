55d0a5d5d1da81d31a497affcee5a2dd0118342d
eli094 <elizabethiscool094@gmail.com>
2024-11-15 19:44:42 -0300

    ArregladasAlgunasCositas

--- Assets/Scripts/Graph/GraphManager.cs
+++ Assets/Scripts/Graph/GraphManager.cs
@@ -6,28 +6,39 @@ using UnityEngine;
 public class GraphManager : MonoBehaviour
 {
     public Graph<VisualNode> graph;
+
     [SerializeField] private GameObject[] verticesPrefab;
     [SerializeField] private GameObject traveler;
     [SerializeField] private TextMeshProUGUI travelCost;
+
     private VisualNode currentDestination;
+
     private Stack<VisualNode> travelToDestination = new Stack<VisualNode>();
+
     private int totalTravelCost;
 
     private void Awake()
     {
         Debug.Log(traveler);
+
         graph = new Graph<VisualNode>();
 
         for (int i = 0; i < verticesPrefab.Length; i++)
         {
             verticesPrefab[i].GetComponentInChildren<TextMeshProUGUI>().text = (i + 1).ToString();
+
             VisualNode visualNode = new VisualNode(i + 1);
+
             graph.AddNodes(visualNode);
         }
+
         NodeConnections();
+
         currentDestination = graph.nodeList[0];
+
         traveler.transform.position = new Vector2 (verticesPrefab[0].transform.position.x, verticesPrefab[0].transform.position.y);
     }
+
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
@@ -49,42 +60,64 @@ public class GraphManager : MonoBehaviour
         graph.AddConnections(graph.nodeList[5], graph.nodeList[6], 8);
         graph.AddConnections(graph.nodeList[4], graph.nodeList[6], 3);
     }
+
     private RaycastHit2D NodeIdentifier()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
         RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity);
 
         return hit;
     }
+
     private void TravelToDestination(RaycastHit2D ray)
     {
         GameObject node = ray.collider.gameObject;
+
         int nodeIndex = int.Parse(node.GetComponentInChildren<TextMeshProUGUI>().text);
+
         VisualNode visualNode = new VisualNode(nodeIndex);
         visualNode = graph.ReturnNodes(visualNode);
+
         totalTravelCost = 0;
         travelToDestination.Push(graph.nodeList[nodeIndex - 1]);
+
         CreatePath(currentDestination, graph.nodeList[nodeIndex - 1]);
+
         if (travelToDestination.Count > 0)
         {
             StartCoroutine(TravelerAnimations());
+        }
+
+        else
+        {
+            Debug.Log("Path not Found :(");
         }
     }
     private IEnumerator TravelerAnimations()
     {
         int visualNodes;
+
         while (travelToDestination.Count > 0)
         {
             visualNodes = travelToDestination.Pop().NodeID;
+
             totalTravelCost += graph.ReturnTravelCost(currentDestination, graph.nodeList[visualNodes - 1]);
+
             Debug.Log(verticesPrefab[visualNodes - 1]);
+
             traveler.transform.position = verticesPrefab[visualNodes - 1].transform.position;
+
             currentDestination = graph.ReturnNodes(graph.nodeList[visualNodes - 1]);
+
             yield return new WaitForSeconds(1f);
         }
+
         Debug.Log(totalTravelCost);
+
         travelCost.text = totalTravelCost.ToString();
     }
+
     private void CreatePath(VisualNode start, VisualNode destination)
     {
         foreach (var connections in graph.adjacencyList)
@@ -96,18 +129,24 @@ public class GraphManager : MonoBehaviour
                     if (connections.Key.NodeID == start.NodeID)
                     {
                         Debug.Log("Doing1");
+
                         return;
                     }
                     else
                     {
                         Debug.Log("Doing2");
+
                         travelToDestination.Push(connections.Key);
+
                         CreatePath(start, connections.Key);
+
                         return;
                     }
                 }
             }
         }
+
+        travelToDestination.Clear();
     }
 }
 
